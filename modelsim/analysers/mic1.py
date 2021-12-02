1cf64f630fb68f11bbdd2aaf03898b2603d2fd9c
Yago C Martins <45719184+ycmr@users.noreply.github.com>
2021-12-01 22:16:31 -0300

    Update mic1.py

--- modelsim/analysers/mic1.py
+++ modelsim/analysers/mic1.py
@@ -10,11 +10,50 @@ import pandas as pd
 # In[2]:
 
 
-df=pd.read_csv('../test-cases/log/mic1_tb.csv', delimiter=';',index_col=False)
+df=pd.read_csv('../test-cases/log/mic1_tb.csv', delimiter=';')
 
 
 # In[3]:
 
+
+def is_setup():
+    flag= False
+    if((clock== '0') 
+       and (rom_data == '00000000000000000000000000000000') 
+       and (ram_data == '00000000000000000000000000000000')
+       and (mir == '0000000000000000')
+       and (c == '0')
+       and (a == 'x')
+       and (b == 'x')
+       and (mar == 'x')
+       and (mdr == 'x')
+       and (pc == 'x')
+       and (mbr == 'x') ):
+        flag= True
+    return flag
+
+
+# In[4]:
+
+
+def is_default():
+    flag= False
+    if((clock == 'x') 
+       and (rom_data == 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx') 
+       and (ram_data == 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+       and (mir == 'xxxxxxxxxxxxxxxx')
+       and (c == 'x')
+       and (a == 'x') and
+       (b == 'x') and 
+       (mar == 'x') and
+       (mdr == 'x') and 
+       (pc == 'x') and
+       (mbr == 'x') ):
+        flag= True
+    return flag
+
+
+# In[5]:
 
 
 for i in range(len(df)):
@@ -32,17 +71,17 @@ for i in range(len(df)):
     mdr=data[9]
     pc=data[10]
     mbr=data[11]
-
-
-# In[4]:
-
-
-print(mbr)
-
-
-# In[ ]:
-
-
-# ['inpections_counter','clock','rom_data','ram_data','mir','c','a','b','mar','mdr','pc',
-# 'mbr']
-
+    
+    if(inpections_counter == 0 and is_default()):
+        print(f"A inicialização em {i+1} está ok")
+  
+    elif(inpections_counter == 0 and not is_default()):
+        print(f"está ok")
+        
+    elif(inpections_counter == 1 and is_setup()):
+        print(f"O setup em {i+1} está OK")
+        
+    elif(inpections_counter == 1 and not is_setup()):
+        print(f"O setup em {i+1} está OK")
+            
+            
